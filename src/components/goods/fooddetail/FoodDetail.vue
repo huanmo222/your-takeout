a5ccd9522f919fc9437f015e58037615083dfa3b
huan_m <929571370@qq.com>
2018-06-19 18:10:18 +0800

    anniushuibowen

---
+++ src/components/goods/fooddetail/FoodDetail.vue
@@ -0,0 +1,25 @@
+<!-- FoodDetail -->
+<template>
+  <div class="food-detail"></div>
+</template>
+
+<script>
+export default {
+  name: 'FoodDetail',
+  props: {
+    food: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  .food-detail
+    
+
+</style>
--- src/components/home/tab/Tab.vue
+++ src/components/home/tab/Tab.vue
@@ -1,9 +1,15 @@
 <!-- Tab -->
 <template>
   <div class="tab border-1px">
-    <router-link to="/goods" tag="div" class="tab-item">商品</router-link>
-    <router-link to="/ratings" tag="div" class="tab-item">评价</router-link>
-    <router-link to="/seller" tag="div" class="tab-item">商家</router-link>
+    <transition name="goods-button">
+      <router-link to="/goods" tag="div" class="tab-item">点餐</router-link>
+    </transition>
+    <transition name="ratings-button">
+      <router-link to="/ratings" tag="div" class="tab-item">评价</router-link>
+    </transition>
+    <transition name="seller-button">
+      <router-link to="/seller" tag="div" class="tab-item">商家</router-link>
+    </transition>
   </div>
 </template>
 
@@ -31,7 +37,15 @@ export default {
       text-align center
       color rgb(77,85,93)
       border-1px(rgba(7,17,27,0.1))
+      &:active
+        background rgba(7,17,27,0.1)
       &.active
         color rgb(240,20,20)
+      &.fold-enter, &.fold-leave-to
+        transform translate3d(0, 100%, 0)
+      &.fold-enter-to, &.fold-leave
+        transform translate3d(0, 0, 0)
+      &.fold-enter-active, &.fold-leave-active
+        transition: all .5s ease-out
 
 </style>
